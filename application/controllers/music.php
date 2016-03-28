94da3b315c32ece1ad98dc27d33fc9369d435107
Hemant Mann <hemant.mann121@gmail.com>
2016-03-28 16:50:05 +0530

    Some changes

--- application/controllers/music.php
+++ application/controllers/music.php
@@ -90,6 +90,8 @@ class Music extends Controller {
             header('Content-Description: File Transfer');
             header('Content-Type: application/octet-stream');
             header('Content-Disposition: attachment; filename="' . basename($title) . '"');
+            header('Expires: 0');
+            header('Cache-Control: must-revalidate');
             header('Pragma: public');
             header('Content-Length: ' . filesize($file));
             readfile($file);
